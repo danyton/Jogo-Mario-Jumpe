e1c355e300e8ffe5ca123a995a4533ecaf49658e
Heverton Freire Barbosa <108628363+danyton@users.noreply.github.com>
2023-05-15 22:29:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const scoreElement = document.querySelector('.score');
+
+let score = 0;
+
+const jump = () => {
+  mario.classList.add('jump');
+
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  }, 550);
+};
+
+const updateScore = () => {
+  score++;
+  scoreElement.innerText = score;
+};
+
+const loop = setInterval(() => {
+  console.log('loop');
+
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './src/imagens/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+  }
+
+  updateScore();
+}, 10);
+
+document.addEventListener('keydown', jump);
